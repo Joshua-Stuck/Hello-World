b5bf9d40b8fce55b6c08613c325de32e51735b1d
Joshua-Stuck <78100236+Joshua-Stuck@users.noreply.github.com>
2021-01-27 12:13:54 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**************************************************************************
+* Java Program Name
+***************************************************************************
+* Joshua Stuck
+* 01/27/2021
+*CMSC 255-003
+**************************************************************************/
+
+public class Hello {
+	public static void main(String[]args) {
+
+		System.out.println("<Joshua Stuck>");
+		System.out.println("<Lab 01>");
+		System.out.println("<Lab 01>");
+		System.out.println("<January 27 2021>");
+		System.out.println("<CMSC 255 003>");
+		System.out.println();
+	}
+}
